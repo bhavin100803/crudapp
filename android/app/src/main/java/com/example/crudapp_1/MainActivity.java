d0890b9b03d50f51bd579179a2070557cf0d1053
Bhavin Makwana <bhavinmakwana293@example.com>
2024-07-26 17:04:47 +0530

    crudapp

---
+++ android/app/src/main/java/com/example/crudapp_1/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.crudapp_1;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pages/employee.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:random_string/random_string.dart';
+
+import '../service/database.dart';
+
+class Employee extends StatefulWidget {
+  const Employee({super.key});
+
+  @override
+  State<Employee> createState() => _EmployeeState();
+}
+
+class _EmployeeState extends State<Employee> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Employee",
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Form",
+              style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(left: 20, top: 30, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Name",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Age",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextField(
+                controller: ageController,
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Location",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextField(
+                controller: locationController,
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+                child: ElevatedButton(
+                    onPressed: () async {
+                      String Id = randomAlphaNumeric(10);
+                      Map<String,dynamic> employeeInfoMap = {
+                        "Name": nameController.text,
+                        "Age": ageController.text,
+                        "Id": Id,
+                        "Location": locationController.text,
+                      };
+                      await DatabaseMethods().addEmployeeDetails(employeeInfoMap, Id).then((value) {
+                        Fluttertoast.showToast(
+                            msg: "Employee details has been uploaded successfully",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.green,
+                            textColor: Colors.white,
+                            fontSize: 16.0
+                        );
+                      });
+                    },
+                    child: Text(
+                      "Add",
+                      style:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )))
+          ],
+        ),
+      ),
+    );
+  }
+}
